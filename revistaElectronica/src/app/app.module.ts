1beaa6b21f92bd8e0f233244640f82d67f050c1a
Natalia Segura <natalia.segurar@autonoma.edu.co>
2021-03-07 11:31:34 -0500

    Añadir los componentes

--- revistaElectronica/src/app/app.module.ts
+++ revistaElectronica/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuNavegacionComponent } from './Components/menu-navegacion/menu-navegacion.component';
+import { ArticulosComponent } from './Components/articulos/articulos.component';
+import { AutoresComponent } from './Components/autores/autores.component';
+import { InformacionComponent } from './Components/informacion/informacion.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuNavegacionComponent,
+    ArticulosComponent,
+    AutoresComponent,
+    InformacionComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
