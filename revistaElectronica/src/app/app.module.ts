f1a3598caa9aa2cf8e807e35cda3313916836fa9
Karol Rojas <karol.rojasb@autonoma.edu.co>
2021-03-18 23:36:20 -0500

    Hacer enlaces al componente registro

--- revistaElectronica/src/app/app.module.ts
+++ revistaElectronica/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
 import { AppComponent } from './app.component'
 import { MenuNavegacionComponent } from './Components/menu-navegacion/menu-navegacion.component'
 import { ArticulosComponent } from './Components/articulos/articulos.component'
@@ -8,10 +7,11 @@ import { AutoresComponent } from './Components/autores/autores.component'
 import { InformacionComponent } from './Components/informacion/informacion.component'
 import { LoginComponent } from './Components/login/login.component'
 import { InicioComponent } from './Components/inicio/inicio.component'
-import { appRouting } from './app.routes';
-import { HeaderComponent } from './Components/header/header.component';
-import { SearchBarComponent } from './Components/search-bar/search-bar.component';
+import { appRouting } from './app.routes'
+import { HeaderComponent } from './Components/header/header.component'
+import { SearchBarComponent } from './Components/search-bar/search-bar.component'
 import { RegistroComponent } from './Components/registro/registro.component'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +23,7 @@ import { RegistroComponent } from './Components/registro/registro.component'
     InicioComponent,
     HeaderComponent,
     SearchBarComponent,
-    RegistroComponent,
+    RegistroComponent
   ],
   imports: [BrowserModule, appRouting],
   providers: [],
