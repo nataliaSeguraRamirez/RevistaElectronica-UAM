b255f774e9b1eed7361e1f3da87f20fc09b40b77
Karol Rojas <karol.rojasb@autonoma.edu.co>
2021-05-27 20:34:05 -0500

    pie de pagina import

--- revistaElectronica/src/app/app.module.ts
+++ revistaElectronica/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { appRouting } from './app.routes'
 import { HeaderComponent } from './Components/header/header.component'
 import { SearchBarComponent } from './Components/search-bar/search-bar.component'
 import { RegistroComponent } from './Components/registro/registro.component'
+import { PieDePaginaComponent } from './Components/pie-de-pagina/pie-de-pagina.component'
 
 @NgModule({
   declarations: [
