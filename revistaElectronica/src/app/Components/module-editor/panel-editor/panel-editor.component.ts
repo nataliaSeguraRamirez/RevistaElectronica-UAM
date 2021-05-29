15d1e416cad121f23d07f1f59171f6b07d3c37d8
nataliaSeguraRamirez <75044430+nataliaSeguraRamirez@users.noreply.github.com>
2021-05-29 09:38:20 -0500

    Rodio buttons

--- revistaElectronica/src/app/Components/module-editor/panel-editor/panel-editor.component.ts
+++ revistaElectronica/src/app/Components/module-editor/panel-editor/panel-editor.component.ts
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { articulos } from 'src/app/Modelos/articulos.interface';
 import { ServidorService } from 'src/app/servidor.service';
-=======
-import { Component, OnInit } from '@angular/core'
-import { articulos } from 'src/app/Modelos/articulos.interface'
-import { ServidorService } from 'src/app/servidor.service'
-import { Router } from '@angular/router'
->>>>>>> master
 
 @Component({
   selector: 'app-panel-editor',
@@ -16,18 +9,24 @@ import { Router } from '@angular/router'
   styleUrls: ['./panel-editor.component.css'],
 })
 export class PanelEditorComponent implements OnInit {
+
+
   public articulos1 = [] as articulos[]
   constructor(private servidor: ServidorService, private router: Router) {
     this.obtenerArticulos()
+
   }
+  ngOnInit(): void {}
 
   public obtenerArticulos() {
     this.servidor.obtenerArticulosTotal().subscribe((data) => {
+      console.log(data)
       this.articulos1 = data as articulos[]
     })
   }
+
   public verArticulo(index: number) {
     console.log(index)
-    this.router.navigate([{ outlets: { editor: ['articulos', index] } }])
+    this.router.navigate([{ outlets: { lec: ['articulo', index] } }])
   }
 }
