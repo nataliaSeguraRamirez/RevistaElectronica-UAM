2a753153fccb615b73efbd82911d3576ac1c9907
Karol Rojas <karol.rojasb@autonoma.edu.co>
2021-05-29 06:23:23 -0500

    Hace el logout

--- revistaElectronica/src/app/Components/logout/logout.component.ts
+++ revistaElectronica/src/app/Components/logout/logout.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+  styleUrls: ['./logout.component.css'],
 })
 export class LogoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    localStorage.setItem('loginAutor', 'false')
+    localStorage.setItem('loginEditor', 'false')
   }
 
+  ngOnInit(): void {}
 }
